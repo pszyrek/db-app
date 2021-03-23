e2a60ce1b1ea8e7855b5e6c7da544b864f4fac50
WBujna <wojciech.bujna@zs6sobieski.edu.pl>
2021-03-23 21:29:15 +0100

    named holidays in holidays file

--- src/assets/holidays.js
+++ src/assets/holidays.js
@@ -1,7 +1,54 @@
 export const holidays = [
     {
-        name: 'nazwa święta',
-        data: 'w formacie DD-MM-YYYY',
+        name: 'Nowy Rok',
+        data: '01.01.2021',
     },
-    {},
+    {
+        name: 'Święto Trzech Króli',
+        data: '06.01.2021',
+    },
+    {
+        name: 'Wielkanoc, pierwszy dzień świąt',
+        data: '04.04.2021',
+    },
+    {
+        name: 'Wielkanoc, drugi dzień świat',
+        data: '05.04.2021',
+    },
+    {
+        name: 'Święto Pracy',
+        data: '01.05.2021',
+    },
+    {
+        name: 'Święto Konstytucji 3 Maja',
+        data: '03.05.2021',
+    },
+    {
+        name: 'Zesłanie Ducha Świętego (Zielone Świątki)',
+        data: '23.05.2021',
+    },
+    {
+        name: 'Boże Ciało',
+        data: '03.06.2021',
+    },
+    {
+        name: 'Wniebowzięcie NMP i Święto Wojska Polskiego',
+        data: '15.07.2021',
+    },
+    {
+        name: 'Dzień Wszystkich Świętych',
+        data: '01.11.2021',
+    },
+    {
+        name: 'Narodowe Święto Niepodległości',
+        data: '11.11.2021',
+    },
+    {
+        name: 'Boże Narodzenie, pierwszy dzień świąt',
+        data: '25.12.2021',
+    },
+    {
+        name: 'Boże Narodzenie, drugi dzień świąt',
+        data: '26.12.2021',
+    }
 ]
