c31abad0e6e4229c31c0ceae4a3f252497a194dd
Piotr Bujna <piotrekbujna@gmail.com>
2021-03-25 19:49:28 +0100

    corrected holidays, added quarters

--- src/assets/holidays.js
+++ src/assets/holidays.js
@@ -1,61 +1,61 @@
 export const holidays = [
     {
         name: 'Nowy Rok',
-        data: '2021-01-01',
+        data: '01-01',
     },
     {
         name: 'Święto Trzech Króli',
-        data: '2021-01-06',
+        data: '01-06',
     },
     {
         name: 'Wielkanoc, pierwszy dzień świąt',
-        data: '2021-04-04',
+        data: '04-04',
     },
     {
         name: 'Wielkanoc, drugi dzień świat',
-        data: '2021-04-05',
+        data: '04-05',
     },
     {
         name: 'Święto Pracy',
-        data: '2021-05-01',
+        data: '05-01',
     },
     {
         name: 'Święto Konstytucji 3 Maja',
-        data: '2021-05-03',
+        data: '05-03',
     },
     {
         name: 'Zesłanie Ducha Świętego (Zielone Świątki)',
-        data: '2021-05-23',
+        data: '05-23',
     },
     {
         name: 'Boże Ciało',
-        data: '2021-06-03',
+        data: '06-03',
     },
     {
         name: 'Wniebowzięcie NMP i Święto Wojska Polskiego',
-        data: '2021-07-15',
+        data: '07-15',
     },
     {
         name: 'Dzień Wszystkich Świętych',
-        data: '2021-11-01',
+        data: '11-01',
     },
     {
         name: 'Narodowe Święto Niepodległości',
-        data: '2021-11-11',
+        data: '11-11',
     },
     {
         name: 'Boże Narodzenie, pierwszy dzień świąt',
-        data: '2021-12-25',
+        data: '12-25',
     },
     {
         name: 'Boże Narodzenie, drugi dzień świąt',
-        data: '2021-12-26',
+        data: '12-26',
     },
 ]
 
 // TEMPLATKA JAK ZMIENIC TO U GÓRY NA TO NA DOLE
 
 // export const holidays = {
-//     'YYYY-MM-DD': 'name of holiday',
+//     'MM-DD': 'name of holiday',
 //     ...
 // }
