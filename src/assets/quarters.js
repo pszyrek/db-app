5d430d9fdded5b791a87aac8a3d92413903bec1b
Piotr Bujna <piotrekbujna@gmail.com>
2021-03-26 21:20:32 +0100

    change structure of quarters file, added some new features to calendar generator

--- src/assets/quarters.js
+++ src/assets/quarters.js
@@ -1,6 +1,13 @@
-export const quarters = {
+export const quartersObject = {
     '12-01': { end: '02-28' },
     '03-01': { end: '05-31' },
     '06-01': { end: '08-31' },
     '09-01': { end: '11-30' },
 }
+
+export const quarters = [
+    [11, 0, 1],
+    [2, 3, 4],
+    [5, 6, 7],
+    [8, 9, 10],
+]
--- src/components/Callendar/controller.js
+++ src/components/Callendar/controller.js
@@ -1,21 +1,50 @@
 import moment from 'moment'
 
+import { holidays } from 'assets/holidays'
+import { quarters } from 'assets/quarters'
+
 export const generateCalendar = () => {
     let months = []
     for (let month = 1; month <= 12; month++) {
-        let days = []
+        const yearAgo = moment().subtract(1, 'years').format('YYYY')
+        let monthObject = { strength: 0, days: [] }
         for (
             let day = 1;
-            day <= moment(`2021-${month}`, 'YYYY-MM').daysInMonth();
+            day <=
+            moment(
+                `${month === 12 && yearAgo}-${month}`,
+                'YYYY-MM'
+            ).daysInMonth();
             day++
         ) {
-            const date = moment(`2021-${month}-${day}`)
-            days.push({
+            const dayWithMonth = moment(`${month}-${day}`).format('MM-DD')
+            const date = moment(`${month === 12 && yearAgo}-${month}-${day}`)
+
+            const isHoliday =
+                !!holidays[dayWithMonth] || date.format('dddd') === 'Sunday'
+
+            if (!isHoliday) {
+                monthObject.strength += 8
+            }
+
+            monthObject.days.push({
                 name: date.format('dddd'),
                 date: date.format('YYYY-MM-DD'),
+                isHoliday,
             })
         }
-        months.push(days)
+        months.push(monthObject)
     }
-    return months
+
+    const quartersStrengths = quarters.reduce((acc, quarter, index) => {
+        quarter.forEach((monthIndex) => {
+            if (!acc[`quarter-${index + 1}`]) {
+                acc[`quarter-${index + 1}`] = months[monthIndex].strength
+            }
+            acc[`quarter-${index + 1}`] += months[monthIndex].strength
+        })
+        return acc
+    }, {})
+
+    return { months, quartersStrengths }
 }
